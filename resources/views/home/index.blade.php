c6a7047733474bd51de7bfa245bef6101e2e2281
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-10-27 22:56:06 +0700

    Pertemuan 1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Belajar Laravel</title>
+</head>
+
+<body>
+    <h1><?= $name ?></h1>
+    <h3><?= $email ?></h3>
+    <p><?= $github ?></p>
+</body>
+
+</html>
